70a6daff505b423feb620b124286fac0e4d0fe00
Tristan Radisson <tristan.radisson@gmail.com>
2018-11-27 19:50:55 +0100

    updated french part

--- generate.sh
+++ generate.sh
@@ -3,5 +3,5 @@
 asciidoctor -a data-uri -a icons -a toc2 -a toclevels=3 -a lang=en -o dist/en/index.html index.adoc
 asciidoctor -a data-uri -a icons -a toc2 -a toclevels=3 -a lang=fr -o dist/fr/index.html index.adoc
 
-asciidoctor-pdf -a lang=en -o dist/en/index.pdf index.adoc
-asciidoctor-pdf -a lang=fr -o dist/fr/index.pdf index.adoc
+asciidoctor-pdf -a pdf-style=./themes/theme.yml -a lang=en -o dist/en/index.pdf index.adoc
+asciidoctor-pdf -a pdf-style=./themes/theme.yml -a lang=fr -o dist/fr/index.pdf index.adoc
