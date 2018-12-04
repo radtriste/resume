52bbdba878feff6ec23247ad4316a37ad630eab7
Tristan Radisson <tristan.radisson@gmail.com>
2018-12-04 22:07:09 +0100

    Corrected small bug with website url

--- generate.sh
+++ generate.sh
@@ -14,7 +14,7 @@ unset outputDir
 unset pdfOutputFile
 
 function generate_html {		
-	htmlCmdArgs="-a stylesheet=$EXEC_DIR/themes/html/$STYLE_HTML -a icons -a toc2 -a toclevels=3 -a lang=$lang -a website_fr=../fr/ -a website_en=../en/  -a website_prefix=$website_prefix -a pdfName=$pdfOutputFile -o $outputDir/index.html index.adoc"
+	htmlCmdArgs="-a stylesheet=$EXEC_DIR/themes/html/$STYLE_HTML -a icons -a toc2 -a toclevels=3 -a lang=$lang -a website_fr=../fr/ -a website_en=../en/  -a website_url=$website_prefix -a pdfName=$pdfOutputFile -o $outputDir/index.html index.adoc"
 	
 	echo "Generate with HTML cmdPrefix = $cmdPrefix and lang = $lang"
 	cmd="asciidoctor $htmlCmdArgs"
@@ -26,7 +26,7 @@ function generate_html {
 }
 
 function generate_pdf {		
-	pdfCmdArgs="-a pdf-style=$EXEC_DIR/themes/pdf/$STYLE_PDF -a lang=$lang -a website_fr=../fr/ -a website_en=../en/ -a website_prefix=$website_prefix -o $outputDir/$pdfOutputFile index.adoc"
+	pdfCmdArgs="-a pdf-style=$EXEC_DIR/themes/pdf/$STYLE_PDF -a lang=$lang -a website_fr=../fr/ -a website_en=../en/ -a website_url=$website_prefix -o $outputDir/$pdfOutputFile index.adoc"
 	
 	echo "Generate with HTML cmdPrefix = $cmdPrefix and lang = $lang"
 	cmd="asciidoctor-pdf $pdfCmdArgs"
@@ -43,10 +43,10 @@ fi
 
 if [[ "$lang" = "en" ]]; then
 	pdfOutputFile="radisson_resume.pdf"
-	website_prefix="resume"
+	website_url="https://resume.tradisson.fr"
 elif [[ "$lang" = "fr" ]]; then
 	pdfOutputFile="radisson_cv.pdf"
-	website_prefix="cv"
+	website_url="https://cv.tradisson.fr"
 else
 	echo "Unknown language ..."
 	return 1
